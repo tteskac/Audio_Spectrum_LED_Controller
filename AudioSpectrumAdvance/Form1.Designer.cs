b1d7e51f45c79e657bde028b7fa38527f6c9f483
tteskac <tteskac@gmail.com>
2017-01-15 16:58:27 +0100

    Form customization and TCP/IP support - TT 15.01.2017

--- AudioSpectrumAdvance/Form1.Designer.cs
+++ AudioSpectrumAdvance/Form1.Designer.cs
@@ -29,89 +29,259 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.spectrum1 = new AudioSpectrumAdvance.Spectrum();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(104, 221);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 1;
-            // 
-            // progressBar2
-            // 
-            this.progressBar2.Location = new System.Drawing.Point(242, 221);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(100, 23);
-            this.progressBar2.TabIndex = 2;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(387, 221);
+            this.comboBox1.Location = new System.Drawing.Point(459, 263);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(262, 21);
             this.comboBox1.TabIndex = 3;
-            // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Location = new System.Drawing.Point(12, 276);
-            this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(1180, 219);
-            this.chart1.TabIndex = 4;
-            this.chart1.Text = "chart1";
             // 
             // timer1
             // 
             this.timer1.Interval = 25;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
+            this.trackBar1.Maximum = 63;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1288, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(12, 38);
+            this.trackBar2.Maximum = 63;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(1288, 45);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Value = 63;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(10, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 18);
+            this.panel1.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(324, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(139, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Communication enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(459, 194);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(464, 63);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IP Device";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(236, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "8888";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "192.168.137.165";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "IP address:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(727, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 72);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Save settings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(7, 43);
+            this.trackBar4.Maximum = 100;
+            this.trackBar4.Minimum = 1;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(426, 45);
+            this.trackBar4.TabIndex = 15;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar4.Value = 10;
+            this.trackBar4.ValueChanged += new System.EventHandler(this.trackBar4_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(269, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "?";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.trackBar4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 194);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(441, 143);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Light preferences";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Aggression (how fast/smooth light respond to sound):";
+            // 
             // elementHost1
             // 
-            this.elementHost1.Location = new System.Drawing.Point(12, 22);
+            this.elementHost1.Location = new System.Drawing.Point(12, 63);
             this.elementHost1.Name = "elementHost1";
             this.elementHost1.Size = new System.Drawing.Size(1288, 125);
             this.elementHost1.TabIndex = 0;
             this.elementHost1.Child = this.spectrum1;
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.panel1);
+            this.groupBox3.Location = new System.Drawing.Point(9, 94);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(273, 43);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "VU meter";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1309, 522);
-            this.Controls.Add(this.chart1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.progressBar2);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(1309, 347);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.elementHost1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "LED Ambient light controller";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Integration.ElementHost elementHost1;
-        private Spectrum spectrum1;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ProgressBar progressBar2;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Timer timer1;
+        private Spectrum spectrum1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- AudioSpectrumAdvance/Form1.cs
+++ AudioSpectrumAdvance/Form1.cs
@@ -7,40 +7,140 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Net;
+using System.Net.Sockets;
+using System.Resources;
 
 namespace AudioSpectrumAdvance
 {
     public partial class Form1 : Form
     {
+
+        Analyzer analyzer;
+        private int _led_value = 255;
+        private string _cmd = "";
+        private string _last_cmd = "";
+        Socket sending_socket;
+        IPAddress send_to_address;
+        IPEndPoint sending_end_point;
+        List<int> _average = new List<int>();
+        
+
         public Form1()
         {
             InitializeComponent();
 
-            analyzer = new Analyzer(progressBar1, progressBar2, spectrum1, comboBox1, chart1);
+            getSettings();
+
+            analyzer = new Analyzer(spectrum1, comboBox1, trackBar1, trackBar2);
             analyzer.Enable = true;
             analyzer.DisplayEnable = true;
-
             
-
-            timer1.Enabled = true;
         }
-        Analyzer analyzer;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
-
-
-
+            try
+            {
+                _led_value = getAverage(analyzer.getAverageValue());
+                panel1.Width = _led_value;
+            
+                _cmd = ":C= " + _led_value + ";";       //This is the command that is sent over network 
+                                                        // to Arduino that reads command in this form ":C= 255;"
+                if (_cmd != _last_cmd)
+                {
+                    _last_cmd = _cmd;
+                    byte[] send_buffer = Encoding.ASCII.GetBytes(_cmd);
+                    sending_socket.SendTo(send_buffer, sending_end_point);
+                }
+            }
+            catch (Exception) { }
         }
 
-        private void chart2_Click(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            if (checkBox1.Checked)
+            {
+                connect();
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                _cmd = ":C= 255;";
+                byte[] send_buffer = Encoding.ASCII.GetBytes(_cmd);
+                sending_socket.SendTo(send_buffer, sending_end_point);
+            }
+        }
 
+        private void connect()
+        {
+            sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            send_to_address = IPAddress.Parse(textBox1.Text);
+            sending_end_point = new IPEndPoint(send_to_address, int.Parse(textBox2.Text));
+        }
+
+
+        private void getSettings()
+        {
+            timer1.Interval = Properties.Settings.Default.timer_interval;
+
+            trackBar1.Value = Properties.Settings.Default.ch_from;
+            trackBar2.Value = Properties.Settings.Default.ch_to;
+            textBox1.Text = Properties.Settings.Default.ip_address;
+            textBox2.Text = Properties.Settings.Default.ip_port;
+
+            trackBar4.Value = Properties.Settings.Default.average_count;
+            label4.Text = trackBar4.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.ch_from = trackBar1.Value;
+            Properties.Settings.Default.ch_to = trackBar2.Value;
+            Properties.Settings.Default.ip_address = textBox1.Text;
+            Properties.Settings.Default.ip_port = textBox2.Text;
+            Properties.Settings.Default.average_count = trackBar4.Value;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if(trackBar1.Value > trackBar2.Value)
+            {
+                trackBar2.Value = trackBar1.Value;
+            }
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            if (trackBar2.Value < trackBar1.Value)
+            {
+                trackBar1.Value = trackBar2.Value;
+            }
+        }
+
+        private int getAverage(int value)
+        {
+            _average.Add(value);
+
+            while (_average.Count > trackBar4.Value)
+            {
+                _average.RemoveAt(0);
+            }
+
+            return (int) (_average.Sum() / _average.Count());
+        }
+
+        private void trackBar4_ValueChanged(object sender, EventArgs e)
+        {
+            label4.Text = trackBar4.Value.ToString();
         }
     }
 }
