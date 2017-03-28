78d7aa580efcf2b7088bb7ccf6745915a4b3b71f
Tomislav Teskač <tteskac@live.com>
2017-03-28 19:39:52 +0200

    Just renaming and stuff.. - TT 28.03.2017

--- AudioSpectrumAdvance/Properties/AssemblyInfo.cs
+++ AudioSpectrumAdvance/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AudioSpectrumAdvance")]
+[assembly: AssemblyTitle("LED Ambient Light Controller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AudioSpectrumAdvance")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("LED Ambient Light Controller")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
